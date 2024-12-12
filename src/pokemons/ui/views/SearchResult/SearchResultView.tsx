4d3869a56e0171a26da6bfc4883ba3df9602dd0a
Tiffany-Dby <tiffany.dby@gmail.com>
2024-12-12 22:54:05 +0100

    update: removed unecessary useEffect

--- src/pokemons/ui/views/SearchResult/SearchResultView.tsx
+++ src/pokemons/ui/views/SearchResult/SearchResultView.tsx
@@ -2,16 +2,11 @@ import useGetPokemon from "@/pokemons/hooks/useGetPokemon";
 import BaseLayout from "@/shared/ui/components/BaseLayout/BaseLayout";
 import { useSearchParams } from "react-router";
 import PokemonDetails from "@/pokemons/ui/components/PokemonDetails/PokemonDetails";
-import { useEffect } from "react";
 
 const SearchResultView = () => {
   console.log("hello");
   const [searchParams] = useSearchParams();
   const query = searchParams.get("search");
-
-  useEffect(() => {
-    console.log("query", query);
-  }, []);
 
   const { pokemon, isLoading, pokemonError } = useGetPokemon(query);
 
